64703f05a7ecfc6bf8b6bedb22103bb3a7c5f56b
Louis Ngatale <louisngatale@gmail.com>
2024-08-04 15:43:27 +0300

    android spacing issue

--- android/src/main/java/cn/jystudio/bluetooth/escpos/RNBluetoothEscposPrinterModule.java
+++ android/src/main/java/cn/jystudio/bluetooth/escpos/RNBluetoothEscposPrinterModule.java
@@ -343,7 +343,7 @@ public class RNBluetoothEscposPrinterModule extends ReactContextBaseJavaModule
             byte[] data = PrintPicture.POS_PrintBMP(mBitmap, width, nMode, leftPadding);
             //  SendDataByte(buffer);
             sendDataByte(Command.ESC_Init);
-            sendDataByte(Command.LF);
+            // sendDataByte(Command.LF);
             sendDataByte(data);
             sendDataByte(PrinterCommand.POS_Set_PrtAndFeedPaper(30));
             sendDataByte(PrinterCommand.POS_Set_Cut(1));
